d893a59a29aa7965769eec7bf60530fe2121d641
frans <franzferdinand.kbarek@gmail.com>
2022-12-30 13:33:58 +0900

    add cond search

--- routes/mendeteksi-alergi-obat.js
+++ routes/mendeteksi-alergi-obat.js
@@ -1,7 +1,23 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.status(200).json("mendeteksi alergi obat");
+  const pasien = req.body.pasien;
+  const obat = req.body.resep[0].obat.toLowerCase();
+  const obatList = ["proris", "aspirin"];
+  let lo = 0;
+  let hi = obatList.length - 1;
+  console.log(hi);
+  while (lo <= hi) {
+    let mid = Math.floor((hi + lo) / 2);
+    if (obatList[mid] == obat) {
+      return res.status(200).json(mid);
+    } else if (obatList[mid] > obat) {
+      res.status(200).json((hi = mid - 1));
+    } else {
+      res.status(200).json((lo = mid + 1));
+    }
+  }
+  return res.status(200).json(lo);
 });
 
 module.exports = router;
